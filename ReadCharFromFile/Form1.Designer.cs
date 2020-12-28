dda932d149e87535da45a205c650dfa132be3693
virot <virotor.vik@gmail.com>
2020-12-29 00:00:08 +0300

    Добавьте файлы проекта.

---
+++ ReadCharFromFile/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ReadCharFromFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFile = new System.Windows.Forms.Button();
+            this.pathToFile = new System.Windows.Forms.TextBox();
+            this.pathToSaveDir = new System.Windows.Forms.TextBox();
+            this.chooseSaveDir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFile
+            // 
+            this.openFile.Location = new System.Drawing.Point(280, 24);
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(75, 23);
+            this.openFile.TabIndex = 0;
+            this.openFile.Text = "Открыть";
+            this.openFile.UseVisualStyleBackColor = true;
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // pathToFile
+            // 
+            this.pathToFile.Location = new System.Drawing.Point(12, 27);
+            this.pathToFile.Name = "pathToFile";
+            this.pathToFile.Size = new System.Drawing.Size(254, 20);
+            this.pathToFile.TabIndex = 1;
+            // 
+            // pathToSaveDir
+            // 
+            this.pathToSaveDir.Location = new System.Drawing.Point(12, 75);
+            this.pathToSaveDir.Name = "pathToSaveDir";
+            this.pathToSaveDir.Size = new System.Drawing.Size(254, 20);
+            this.pathToSaveDir.TabIndex = 2;
+            // 
+            // chooseSaveDir
+            // 
+            this.chooseSaveDir.Location = new System.Drawing.Point(280, 76);
+            this.chooseSaveDir.Name = "chooseSaveDir";
+            this.chooseSaveDir.Size = new System.Drawing.Size(75, 23);
+            this.chooseSaveDir.TabIndex = 3;
+            this.chooseSaveDir.Text = "Выбрать";
+            this.chooseSaveDir.UseVisualStyleBackColor = true;
+            this.chooseSaveDir.Click += new System.EventHandler(this.button_ChooseSaveDir);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Путь к корневой директории ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Место сохранения результата";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(280, 105);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Старт";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 138);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chooseSaveDir);
+            this.Controls.Add(this.pathToSaveDir);
+            this.Controls.Add(this.pathToFile);
+            this.Controls.Add(this.openFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openFile;
+        private System.Windows.Forms.TextBox pathToFile;
+        private System.Windows.Forms.TextBox pathToSaveDir;
+        private System.Windows.Forms.Button chooseSaveDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ReadCharFromFile/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReadCharFromFile
+{
+    public partial class Form1 : Form
+    {
+
+        private string pathToRootDirectory = "";
+        private string pathToSaveDirectory = "";
+        public Form1()
+        {
+            InitializeComponent();
+            ToolTip toolTip = new ToolTip();
+            toolTip.AutoPopDelay = 5000;
+            toolTip.InitialDelay = 1000;
+            toolTip.ReshowDelay = 500;
+
+            toolTip.ShowAlways = true;
+
+
+            toolTip.SetToolTip(this.label2, "Если не задан, используется путь корневой директории");
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            pathToRootDirectory = OpenFile();
+            pathToFile.Text = pathToRootDirectory;
+        }
+
+       
+
+        private void button_ChooseSaveDir(object sender, EventArgs e)
+        {
+            pathToSaveDirectory = OpenFile();
+            pathToSaveDir.Text = pathToSaveDirectory;
+        }
+
+
+
+        private string OpenFile()
+        {
+            string dirPath = "";
+            using (var openFileDialog = new FolderBrowserDialog())
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    dirPath = openFileDialog.SelectedPath;
+                }
+            }
+
+            return dirPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pathToSaveDirectory.Equals(string.Empty))
+            {
+                pathToSaveDirectory = pathToRootDirectory;
+                pathToSaveDir.Text = pathToSaveDirectory;
+            }
+
+        }
+    }
+}
